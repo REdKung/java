ba4b69300f4c9e51c35a20e7616691d199acb346
REdKung <48342795+REdKung@users.noreply.github.com>
2019-06-19 17:03:34 +0800

    Add files via upload

---
+++ TWID/src/identityNnumber/identity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package identityNnumber;
+
+/**
+ * @author SA
+ *
+ */
+public class identity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		
+	}
+
+}
